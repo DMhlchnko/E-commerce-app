e2e29de0bf211d4b66258e1491833017d8df53ae
DMhlchnko <dan.2000.uami@gmail.com>
2021-10-04 16:47:07 +0300

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Core.Interfaces;
+using Core.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProductsAsync()
+        {
+            var result = await _productRepository.GetProductsAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductByIdAsync(int id)
+        {
+            var result = await _productRepository.GetProductByIdAsync(id);
+
+            return Ok(result);
+        }
+    }
+}
